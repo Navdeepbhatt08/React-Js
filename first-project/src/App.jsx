a62dc7ecced9973549a1cd87720bbccf9eb595a4
NAVDEEP BHATT <NAVDEEP9557@gmail.com>
2025-03-17 21:50:07 +0530

    Tailwind

---
+++ first-project/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to CodDev.com by Navdeep Bhatt !!</h1>
+        <h2>Here , You Will Find all the Code Related to html,css, Js and Full Stack.</h2>
+      </div>
+         <div>
+         <a href="About Us">About Us</a><span>      </span>
+         <a href="S">Source Code</a><span>      </span>
+         <a href="p">Personal</a><span>      </span>
+         <a href="c">Contact Us</a><span>      </span>
+
+         </div>
+    </>
+  )
+}
+
+export default App
